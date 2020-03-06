b707c93cde3ff9ee69472a9b545bdede2bef4d30
monitor monitor <monitor@labinfo.is.escuelaing.edu.co>
2020-03-06 14:19:48 -0500

    Creacion de clase de solucion

---
+++ Proyecto Inicial/checkers por aprobar/CheckersContest.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class CheckersContest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CheckersContest
+{
+    public String[] solve(char player, String[] moves){
+        return null;
+    }
+    
+    public void simulate(char player, String[] moves, boolean slow){
+        
+    } 
+}
