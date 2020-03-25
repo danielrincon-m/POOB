5104a2c30079da2e9aa51490bf0a0b878e39765f
Danielrincon <daniel.rincon96@gmail.com>
2020-03-25 01:00:54 -0500

    Tengo sueño y np quiero hacer mas, pero nos falta reeestooooo

---
+++ Laboratorio 4/SINAP/src/pruebas/SinapTest.java
@@ -0,0 +1,23 @@
+import aplicacion.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SinapTest {
+
+    @Test
+    public void deberiaAdicionarLaNuevaArea() {
+        Sinap sinap = new Sinap();
+        String nombre = "Tuparro";
+        String name = "Tuparro National Park";
+        String ubicacion = "Vichada";
+        String area = "548.000";
+        String descripcion = "Es una extensa sabana verde surcada...";
+        sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        assertEquals(nombre, sinap.getDetalles(nombre, name).getNombre());
+        assertEquals(name, sinap.getDetalles(nombre, name).getName());
+        assertEquals(ubicacion, sinap.getDetalles(nombre, name).getUbicacion());
+        assertEquals(area, sinap.getDetalles(nombre, name).getArea());
+        assertEquals(descripcion, sinap.getDetalles(nombre, name).getDescripcion());
+    }
+}
