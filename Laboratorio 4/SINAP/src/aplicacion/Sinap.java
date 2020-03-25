5cad87ffbf76ca715af32e640d3cf586e9d47a84
Danielrincon <daniel.rincon96@gmail.com>
2020-03-25 15:52:10 -0500

    Codigoooo

--- Laboratorio 4/SINAP/src/aplicacion/Sinap.java
+++ Laboratorio 4/SINAP/src/aplicacion/Sinap.java
@@ -24,7 +24,7 @@ public class Sinap {
     /**
      * Adiciona cinco areas
      */
-    public void adicioneCinco() {
+    public void adicioneCinco() throws SINAPExcepecion{
         Area ejemplos[] = {
                 new Area("Ciénaga Grande de Santa Marta", "Large Marsh of Saint Martha", "Madalena", "4280",
                         "Santuario de flora y fauna." +
@@ -32,23 +32,23 @@ public class Sinap {
                                 " con el fin de garantizar el estado de conservación de la biodiversidad de flora y fauna y la productividad pesquera," +
                                 " para mantener los flujos de nutrientes y cumplir con los protocolos internacionales de Reserva de Biosfera y " +
                                 " humedal Ramsar."),
-                new Area("Nevado del Huila", "Nevado del Huila", " Huila, Tolima, Cauca", "1.580 km²",
+                new Area("Nevado del Huila", "Nevado del Huila", " Huila, Tolima, Cauca", "1.580",
                         "Parque natural" +
                                 " Es considerada estratégica ya que abastece las dos cuencas mas importantes del país " +
                                 "(Cuenca alta del Río Magdalena y Cuenca alta del Río Cauca) catalogándola como una estrella hídrica " +
                                 "del macizo colombiano, que aporta bienes y servicios ambientales representados en ecosistemas de Páramo," +
                                 "subparamo, bosque Andino y altoandino, favoreciendo asi la viabilidad de especies de flora y fauna."),
-                new Area("Laguna de Sonso", "Laguna de Sonso", "Valle del Cauca", " 14,1 km²",
+                new Area("Laguna de Sonso", "Laguna de Sonso", "Valle del Cauca", " 14.1",
                         "Reserva natural," +
                                 "Aves migratorias que durante el invierno viajan desde Norteamérica en busca de buen clima " +
                                 "y turistas de todo el mundo encuentran en la Reserva Natural Laguna de Sonso, 10 km al suroccidente de Buga, " +
                                 "un sitio apacible moldeado por el río Cauca que cuenta con una torre de observación de 10 m de altura."),
-                new Area("Morichales de Paz de Ariporo", "Morichales de Paz de Ariporo", "Casanare", "",
+                new Area("Morichales de Paz de Ariporo", "Morichales de Paz de Ariporo", "Casanare", "2400",
                         "Propuesta de parque natural" +
                                 " El ecosistema lo integra un humedal de 25 mil hectáreas donde impera la variedad de palma de moriche " +
                                 "que sirve de refugio a buena parte de la fauna llanera y donde igualmente residen unas 200 familias que por décadas " +
                                 "han habitado este paradisíaco lugar."),
-                new Area("Parque Isla de Salamanca", "Salamanca Island Road Park", "Magalena", " 	562 km²",
+                new Area("Parque Isla de Salamanca", "Salamanca Island Road Park", "Magalena", " 	562",
                         "Vía parque'." +
                                 "Posee 98 especies de invertebrados, 9 especies de anfibios, 35 especies de reptiles, más de 140 especies de peces, " +
                                 "199 de aves, muchas de ellas migratorias, endémicas y residentes. La existencia de 33 especies de mamíferos indica " +
@@ -90,7 +90,7 @@ public class Sinap {
      * @param ubicacion
      * @param descripcion
      */
-    public void adicione(String nombre, String name, String ubicacion, String area, String descripcion) {
+    public void adicione(String nombre, String name, String ubicacion, String area, String descripcion) throws SINAPExcepecion{
         adicioneDetalles(new Area(nombre, name, ubicacion, area, descripcion));
     }
 
@@ -99,8 +99,14 @@ public class Sinap {
      *
      * @param detalles El detalle asociado con la area
      */
-    public void adicioneDetalles(Area detalles) {
+    public void adicioneDetalles(Area detalles) throws SINAPExcepecion{
         int i = 0;
+        //El área no se debe repetir
+        for (Area area : areas){
+            if (area.getNombre().compareToIgnoreCase(detalles.getNombre()) == 0) {
+                throw new SINAPExcepecion(SINAPExcepecion.AREA_DUPLICADA);
+            }
+        }
         while ((i < areas.size()) && (areas.get(i).getNombre().compareToIgnoreCase(detalles.getNombre()) < 0)) {
             i++;
         }
@@ -115,7 +121,7 @@ public class Sinap {
      * @return Los detalles encontrados
      */
     public ArrayList<Area> busque(String prefijo) {
-        ArrayList<Area> resultados = null;
+        ArrayList<Area> resultados = new ArrayList<Area>();
         prefijo = prefijo.toUpperCase();
         for (int i = 0; i < areas.size(); i++) {
             if (areas.get(i).getNombre().toUpperCase().startsWith(prefijo)) {
--- Laboratorio 4/SINAP/src/pruebas/SinapTest.java
+++ Laboratorio 4/SINAP/src/pruebas/SinapTest.java
@@ -1,20 +1,36 @@
 import aplicacion.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SinapTest {
 
+    private String nombre;
+    private String name;
+    private String ubicacion;
+    private String area;
+    private String descripcion;
+
+    private Sinap sinap;
+
+    @BeforeEach
+    public final void setUp(){
+        sinap = new Sinap();
+        nombre = "Tuparro";
+        name = "Tuparro National Park";
+        ubicacion = "Vichada";
+        area = "548.000";
+        descripcion = "Es una extensa sabana verde surcada...";
+    }
+
     @Test
     public void deberiaAdicionarLaNuevaArea() {
-        Sinap sinap = new Sinap();
-        String nombre = "Tuparro";
-        String name = "Tuparro National Park";
-        String ubicacion = "Vichada";
-        String area = "548.000";
-        String descripcion = "Es una extensa sabana verde surcada...";
-
-        sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        } catch (Exception e) {
+            fail("Ocurrió una excepcion");
+        }
         assertEquals(nombre, sinap.getDetalles(nombre, name).getNombre());
         assertEquals(name, sinap.getDetalles(nombre, name).getName());
         assertEquals(ubicacion, sinap.getDetalles(nombre, name).getUbicacion());
@@ -24,38 +40,93 @@ public class SinapTest {
 
     @Test
     public void deberiaListarTodasLasAreas() {
-        Sinap sinap = new Sinap();
-        String nombre = "Tuparro";
-        String name = "Tuparro National Park";
-        String ubicacion = "Vichada";
-        String area = "548.000";
-        String descripcion = "Es una extensa sabana verde surcada...";
-
-        sinap.adicione(nombre, name, ubicacion, area, descripcion);
         String ans = "Tuparro\n" +
                 "Tuparro National Park\n" +
                 "Es una extensa sabana verde surcada...\n\n";
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        } catch (Exception e) {
+            fail("Ocurrió una excepcion");
+        }
         assertEquals(ans, sinap.toString());
     }
 
     @Test
-    public void sinapPruebaDeAceptacion(){
-        Sinap sinap = new Sinap();
-        String nombre = "Tuparro";
-        String name = "Tuparro National Park";
-        String ubicacion = "Vichada";
-        String area = "548.000";
-        String descripcion = "Es una extensa sabana verde surcada...";
+    public void sinapPruebaDeAceptacion() {
         String ans = "Tuparro\n" +
                 "Tuparro National Park\n" +
                 "Es una extensa sabana verde surcada...\n\n";
 
-        sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+        } catch (Exception e) {
+            fail("Ocurrió una excepcion");
+        }
         assertEquals(nombre, sinap.getDetalles(nombre, name).getNombre());
         assertEquals(name, sinap.getDetalles(nombre, name).getName());
         assertEquals(ubicacion, sinap.getDetalles(nombre, name).getUbicacion());
         assertEquals(area, sinap.getDetalles(nombre, name).getArea());
         assertEquals(descripcion, sinap.getDetalles(nombre, name).getDescripcion());
         assertEquals(ans, sinap.toString());
+    }
+
+    @Test
+    public void deberiaLanzarExcepcionSiNoHayNombreInternacional() {
+        name = "";
+
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            fail("Adicionó área sin nombre internacional");
+        } catch (SINAPExcepecion e) {
+            assertEquals(SINAPExcepecion.SIN_NOMBRE_INTERNACIONAL, e.getMessage());
+        }
+    }
+
+    @Test
+    public void deberiaLanzarExcepcionSiElAreaEstaDuplicada() {
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            fail("Adicionó área dos veces");
+        } catch (SINAPExcepecion e) {
+            assertEquals(SINAPExcepecion.AREA_DUPLICADA, e.getMessage());
+        }
+    }
+
+    @Test
+    public void deberiaLanzarExcepcionSiElAreaNoEsNumerica() {
+        String area = "548.000 Millones";
+
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            fail("Adicionó área con área no numérica");
+        } catch (SINAPExcepecion e) {
+            assertEquals(SINAPExcepecion.AREA_NO_NUMERICA, e.getMessage());
+        }
+    }
+
+    @Test
+    public void deberiaLanzarExcepcionSiLaDescripcionEsMuyCorta() {
+        String descripcion = "Sabana";
+
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            fail("Adicionó área con descripcion muy corta");
+        } catch (SINAPExcepecion e) {
+            assertEquals(SINAPExcepecion.DESCRIPCION_MUY_CORTA, e.getMessage());
+        }
+    }
+
+    @Test
+    public void deberiaEncontrarUnArea() {
+        String ans = "Tuparro\n" +
+                "Tuparro National Park\n" +
+                "Es una extensa sabana verde surcada...";
+        try {
+            sinap.adicione(nombre, name, ubicacion, area, descripcion);
+            assertEquals(ans, sinap.busque("T").get(0).toString());
+        } catch (Exception e) {
+            fail ("Ocurrió una excepcion");
+        }
     }
 }
