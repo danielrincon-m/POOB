85a898efc95dac2e802ec2a680f8a7f18edb5b68
PaulaSanchez810 <46799459+PaulaSanchez810@users.noreply.github.com>
2020-05-11 12:57:07 -0500

    ...

--- Proyecto final/POOng/src/presentacion/CharactersScreen.java
+++ Proyecto final/POOng/src/presentacion/CharactersScreen.java
@@ -4,13 +4,19 @@ import aplicacion.game.enums.CharacterPersonality;
 
 import aplicacion.game.enums.CharacterType;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Enumeration;
 
 public class CharactersScreen extends Screen {
+    public static final String fondoInicial = "resources/fondo3.png";
+    private BufferedImage fondo;
     private CharacterPersonality characterProperties;
     private ButtonGroup botones;
     private JRadioButton opciones,q,e,r,b;
@@ -25,15 +31,26 @@ public class CharactersScreen extends Screen {
         //acciones();
     }
 
+    private void setFondo(String inical) {
+        try {
+            fondo = ImageIO.read(new File(inical));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void prepareElements() {
-        setBorder(new EmptyBorder(200, 200, 280, 250));
+        setFondo(CharactersScreen.fondoInicial);
+        setBorder(new EmptyBorder(200, 200, 280, 100));
         setLayout(new GridLayout(1, 2, 10, 10));
         seleccion = new JPanel();
         seleccion.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(0, 10, 0, 0),new TitledBorder("Selección de personaje")));
         seleccion.setLayout(new GridLayout(0, 1, 5, 5));
+        seleccion.setBackground(new Color(238,255,254));
         imagenes = new JPanel();
-        imagenes.setBorder(new EmptyBorder(10, 60, 0, 20));
+        imagenes.setOpaque(false);
+        imagenes.setBorder(new EmptyBorder(40, 5, 0, 5));
         botones = new ButtonGroup();
         aceptar = new Button("Aceptar");
         atras = new Button("atrás");
@@ -48,9 +65,7 @@ public class CharactersScreen extends Screen {
 
                 imagen = new ImageIcon(playerCharacter.spritePath());
                 Image captura = imagen.getImage();
-
                 Image newimg = captura.getScaledInstance(220,220,Image.SCALE_DEFAULT);
-
                 imagen = new ImageIcon(newimg);
                 personajes = new JLabel (imagen);
                 imagenes.removeAll();
@@ -67,6 +82,7 @@ public class CharactersScreen extends Screen {
         for (CharacterPersonality playerCharacter : CharacterPersonality.values()) {
             if (playerCharacter.getType().equals(CharacterType.HUMAN)) {
                 JRadioButton name = new JRadioButton (playerCharacter.getName());
+                name.setBackground(new Color(91,183,197));
                 name.addActionListener(e1 -> acciones(playerCharacter));
                 botones.add(name);
                 seleccion.add(name);
@@ -78,4 +94,9 @@ public class CharactersScreen extends Screen {
     protected void prepareAccionesElemento() {
 
     }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
+    }
+
 }
